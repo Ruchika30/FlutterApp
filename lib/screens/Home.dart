67763ab15b59bcd9487286f79c358859b83c35af
prasadPegasusa <ruchika.pegasus@gmail.com>
2020-05-16 15:05:38 +0530

    Taking height,width of child widget

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -4,12 +4,18 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget{
    @override
    Widget build(BuildContext context){
-    return Container(
+    return Center(
+      child:Container(
       alignment: Alignment.center,
       color: Colors.yellow,
+      height:200.0,
+      width: 100.0,
       child:Text("This is Home class", textDirection: TextDirection.ltr,)
-
-
+    )
     );
   }
 }
+
+// By default the container takes the height,width of parent
+// But some widgets give pririty to child's height,width like CENTER widget.
+// Hence we have wrapped container in Center widget
