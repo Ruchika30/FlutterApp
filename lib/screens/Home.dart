ca8aa829f6f3dadfd459a1feab49d62fcfeaad05
prasadPegasusa <ruchika.pegasus@gmail.com>
2020-05-16 18:31:53 +0530

    Row widget addde

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -11,15 +11,40 @@ class Home extends StatelessWidget{
       // height:200.0,
       // width: 100.0,
       margin:EdgeInsets.only(left: 20.0, top:20.0),
-      child:Text("This is Home class", 
-      textDirection: TextDirection.ltr,
-      style: TextStyle(
-        color: Colors.red,
-        fontSize: 50.0,
-        fontWeight: FontWeight.w700
-      ),
-      
+
+      child: Row(
+        children: <Widget>[
+           Expanded(child: Text("You", 
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+              color: Colors.red,
+              fontSize: 50.0,
+              fontWeight: FontWeight.w700
+          ),
+          )),
+          Expanded(child: Text("Are", 
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+              color: Colors.red,
+              fontSize: 20.0,
+              fontWeight: FontWeight.w700
+          ),
+          )),
+          Expanded(child:Text("Aweosme", 
+              textDirection: TextDirection.ltr,
+              style: TextStyle(
+              color: Colors.red,
+              fontSize: 30.0,
+              fontWeight: FontWeight.w700
+          ),
+          )),
+          
+        ],
       )
+      
+      
+      
+     
     )
     );
   }
